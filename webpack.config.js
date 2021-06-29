68f3e69041e44bcc7a955e1f4d2bd0c20bd4cdba
Santeri Hurnanen <santeri@oikeuttaelaimille.fi>
2021-06-29 03:14:35 +0300

    Update node packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,38 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const slsw = require("serverless-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const entries = {};
 
-Object.keys(slsw.lib.entries).forEach(
-  key => {
-    // Exclud python files
-    if (!slsw.lib.entries[key].match(/\.py$/)) {
-      entries[key] = ['./source-map-install.js', slsw.lib.entries[key]]
-    }
+Object.keys(slsw.lib.entries).forEach((key) => {
+  // Exclud python files
+  if (!slsw.lib.entries[key].match(/\.py$/)) {
+    entries[key] = ["./source-map-install.js", slsw.lib.entries[key]];
   }
-);
+});
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: entries,
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
-    new CopyPlugin([
-      './*.py'
-    ])
+    new CopyPlugin({
+      patterns: [{ from: "./*.py" }],
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
   },
   output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
   },
-  target: 'node',
+  target: "node",
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ]
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
   },
 };
